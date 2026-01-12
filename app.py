bf514e41ad229a4f34b859ef40a81cc8548c433b
shinnwlfrd <nauwegloru@gmail.com>
2026-01-12 10:59:09 +0900

    Change import from xlsxwriter to XlsxWriter

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 import io
-from xlsxwriter import Workbook
+from XlsxWriter import Workbook
 from datetime import datetime
 import plotly.express as px
 import plotly.graph_objects as go
